bbd41d1830696b13fa817abd8e94f93c45f2355d
Rafael Andrade <arkikis181@gmail.com>
2020-09-30 01:11:02 -0300

    Configurando leitura do swagger

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,13 @@ import bodyParser = require("body-parser");
 import cors = require("cors");
 import routes from "./routes/index.routes";
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 // create and setup express app
 const app = express();
+
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(bodyParser.json());
 
